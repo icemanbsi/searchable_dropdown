bad9412c3c27d007ca901813138bbd7cfb4cc90b
matheusfajardo <matheus.rof@gmail.com>
2020-06-27 12:50:20 -0300

    Return old value after first select. #55

--- lib/searchable_dropdown.dart
+++ lib/searchable_dropdown.dart
@@ -443,6 +443,13 @@ class _SearchableDropdownState<T> extends State<SearchableDropdown<T>> {
 
   @override
   void initState() {
+   if (selectedItems == null) selectedItems = [];
+   if (widget.value != null) {
+      int i = indexFromValue(widget.value);
+      if (i != null && i != -1) {
+        selectedItems = [i];
+      }
+    }
     _updateSelectedIndex();
     super.initState();
   }
@@ -453,13 +460,7 @@ class _SearchableDropdownState<T> extends State<SearchableDropdown<T>> {
     }
     if (widget.multipleSelection) {
       selectedItems = List<int>.from(widget.selectedItems ?? []);
-    } else if (widget.value != null) {
-      int i = indexFromValue(widget.value);
-      if (i != null && i != -1) {
-        selectedItems = [i];
-      }
-    }
-    if (selectedItems == null) selectedItems = [];
+    }     
   }
 
   @override
